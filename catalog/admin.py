a62c6f1685d47fae5970aa4c2e162942ca997bcc
Felipe <felipe.viera@mayor.cl>
2026-04-24 11:30:30 -0400

    Registré los modelos en admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author,Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    '''
+    Configuración del admin para autores.
+    '''
+    
+    list_display = ("id", "name", "country", "created_at")
+    search_fields = ("name", "country")
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    '''
+    Configuración del admin para libros.
+    '''
+
+    list_display = ("id", "title", "author", "genre_main", "available_in_spanish", "is_active")
+    list_filter = ("genre_main", "available_in_spanish", "is_active")
+    search_fields = ("title", "author__name", "isbn")
